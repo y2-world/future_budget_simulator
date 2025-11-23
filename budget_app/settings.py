85381ea107b0b9618013cfa25d577d2b432135bd
Yuki <yuki92496@gmail.com>
2025-11-23 11:53:33 +0900

    Fix ALLOWED_HOSTS handling

--- budget_app/settings.py
+++ budget_app/settings.py
@@ -6,9 +6,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
-STATICFILES_DIRS = [
-    str(BASE_DIR / 'static'),
-]
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 # WhiteNoise
 MIDDLEWARE = [
@@ -18,5 +16,9 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# ALLOWED_HOSTS を Heroku 環境変数から取得、存在しない場合は '*' にして全ホスト許可
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+# ALLOWED_HOSTS
+env_hosts = os.environ.get('ALLOWED_HOSTS', '*')
+ALLOWED_HOSTS = [h.strip() for h in env_hosts.split(',') if h.strip()]
+
+# デバッグ用に Heroku で値を確認
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
